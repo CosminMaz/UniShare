7c65f123af872669386fd78a252ba9b74895894d
rebecacojocarescu <161582865+rebecacojocarescu@users.noreply.github.com>
2025-11-13 19:46:32 +0200

    Register page

--- Frontend/UniShare/src/App.jsx
+++ Frontend/UniShare/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
 import LoginPage from './pages/Login'
+import RegisterPage from './pages/Register'
 
 function App() {
+  const path = window.location.pathname
+
+  if (path === '/register') {
+    return <RegisterPage />
+  }
+
   return <LoginPage />
 }
 
