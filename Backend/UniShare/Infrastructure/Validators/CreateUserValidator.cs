c62bda0e367a32480343deb2101df921afca6496
Mardare Andrei Daniel <mardareandreidaniel@Mardares-MacBook-Air.local>
2025-12-17 19:38:22 +0200

    fix

--- Backend/UniShare/Infrastructure/Validators/CreateUserValidator.cs
+++ Backend/UniShare/Infrastructure/Validators/CreateUserValidator.cs
@@ -10,10 +10,6 @@ public class CreateUserValidator:AbstractValidator<RegisterUserRequest>
         RuleFor(x => x.Email).NotNull().NotEmpty().EmailAddress().WithMessage("A valid email is required.");
         RuleFor(x => x.Password)
             .NotNull().NotEmpty().WithMessage("Password is required.")
-            .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
-            .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
-            .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
-            .Matches("[0-9]").WithMessage("Password must contain at least one digit.")
-            .Matches("[!@#$%^&*()]").WithMessage("Password must contain at least one special character.");
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
     }
 }
